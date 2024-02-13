510719f847856a660e9c9cb3d5519d81b7bb1937
saadat <saadatummulkhair@gmali.com>
2024-02-13 15:35:28 +0100

    saved

---
+++ app.js
@@ -0,0 +1,9 @@
+let main= document .querySelector(' .main')
+let form=document.querySelector('form')
+main . addEventListener('mouseover' , function(){
+    setTimeout(()=>{
+
+    form .style.display="block"
+
+    },5000 )
+})
